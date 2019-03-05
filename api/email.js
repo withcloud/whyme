153cd37708154d3ada03f21047dbb213c9f7f59c
Comus Leong <comus.leong@gmail.com>
2019-03-06 02:37:00 +0800

    add api email

---
+++ api/email.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const Parser = require('rss-parser')
+const moment = require('moment-timezone')
+const postmark = require('postmark')
+
+moment.tz.setDefault('Asia/Macau')
+const app = express()
+const parser = new Parser()
+const client = new postmark.ServerClient(process.env.POSTMARK_SERVER_TOKEN)
+
+const sendEmail = async (req, res) => {
+  const { emails = [] } = req.body
+
+  const feed = await parser.parseURL('https://whyme.dev/rss.xml')
+
+  const startTime = moment('Thu, 28 May 2015 22:40:32 GMT') // .startOf('day')
+  const posts = feed.items.filter(item => {
+    return moment(item.isoDate).isSameOrAfter(startTime)
+  })
+
+  const messages = []
+  posts.forEach(post => {
+    const content = post['content:encoded']
+    const link = post.link
+    const title = post.title
+    emails.forEach(email => {
+      messages.push({
+        From: 'WithCloud Why Me <whyme@withcloud.co>',
+        To: email,
+        TemplateAlias: 'post',
+        TemplateModel: {
+          link,
+          content,
+          title
+        },
+        InlineCss: true
+      })
+    })
+  })
+
+  if (messages.length) {
+    await client.sendEmailBatchWithTemplates(messages)
+  }
+
+  res.end('OK')
+}
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(sendEmail)
+
+module.exports = app
